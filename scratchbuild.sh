b683a2b9150d846d282d478bc4f89af462659b10
Fabio Madge-Pimentel <fabio@madge.me>
2013-11-04 23:18:02 +0100

    intoducing costom graphics, Hyberbel still missing.

--- scratchbuild.sh
+++ scratchbuild.sh
@@ -1,3 +1,11 @@
-rm *.aux *.bbl *.blg *.log *.pdf *.toc
-texi2pdf Seminararbeit.tex
-open Seminararbeit.pdf
+rm *.aux *.bbl *.blg *.log *.pdf *.toc *.dvi *.ps *.out
+latex Seminararbeit.tex
+bibtex Seminararbeit.aux
+latex Seminararbeit.tex
+latex Seminararbeit.tex
+dvips Seminararbeit.dvi
+ps2pdf14 Seminararbeit.ps
+
+if [ "$1" == 'open' ]; then
+       open Seminararbeit.pdf 
+fi
